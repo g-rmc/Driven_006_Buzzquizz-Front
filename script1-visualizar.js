0a241ae000e071dd1a8bf237f363648e6c4da3e6
g-rmc <g.rmc3000@gmail.com>
2022-06-29 19:39:29 -0300

    Tela inicial e carregamento dos quizzes

--- script1-visualizar.js
+++ script1-visualizar.js
@@ -1,3 +1,79 @@
+let numQuizzes = 0;
+const paginaBase = document.querySelector(".paginaQuizz");
+
 function atualizarPagina(){
-    document.location.reload(true);
+    document.location.reload();
 }
+
+function carregarPaginaInicial(){
+
+    paginaBase.innerHTML = "";
+
+    paginaBase.innerHTML = `  <div class="meusQuizzes">
+                        <h3>Você não criou nenhum</br>quizz ainda :(</h3> 
+                        <button onclick="paginaComeco()">Criar Quizz</button>
+                    </div>
+
+                    <div class="todosQuizzes">
+                        <h1>Todos os Quizzes</h1>
+                        <div class="quizzes"></div>
+                    </div>`
+
+    carregarQuizzes();
+}
+
+function carregarQuizzes(){
+    let promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+    promise.then(renderizarQuizzes);
+    promise.catch(alertaErro);
+}
+
+function renderizarQuizzes(obj){
+    let array = obj.data;
+    numQuizzes = array.length;
+    
+    let divQuizzes = document.querySelector(".quizzes");
+
+    divQuizzes.innerHTML = ""
+
+    console.log(array[1])
+
+    for (let i = 0; i < numQuizzes; i++){
+        
+        let quizz = array[i];
+
+        let html =  `   <div class="quizz"
+                        style="background-image: url('${quizz.image}');"
+                        onclick="carregarQuizz(${quizz.id})">
+                            <div>${quizz.title}</div>
+                        </div>`;
+
+        divQuizzes.innerHTML += html;
+    }
+}
+
+function alertaErro(obj){
+    let id = obj.status
+    console.log(`Erro ${id}`);
+    alert(`Erro ${id}`);
+}
+
+function carregarQuizz(id){
+    let promise = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`);
+    promise.then(renderizarQuizz);
+    promise.catch(alertaErro);
+}
+
+function renderizarQuizz(obj){
+
+    let quizz = obj.data;
+
+    paginaBase.innerHTML = "";
+
+    paginaBase.innerHTML = `<div class="todosQuizzes">${quizz.title}</div>`;
+
+    console.log(quizz)
+
+}
+
+carregarPaginaInicial();
--- script2-criar.js
+++ script2-criar.js
@@ -1,27 +1,30 @@
 let pag1 = document.querySelector('.paginaQuizz');
-var tituloValue=undefined;
-var urlValue=undefined;
-var qtdeValue=undefined;
-var nivelValue=undefined;
-var contadorPerguntas=0;
+let tituloValue=undefined;
+let urlValue=undefined;
+let qtdeValue=undefined;
+let nivelValue=undefined;
+let contadorPerguntas=0;
 
 function paginaComeco(){
-pag1.innerHTML += `<div class="topo"><h1>BuzzQuizz</h1></div>
+
+pag1.innerHTML = "";
+
+pag1.innerHTML += `
                     <div class="enunciado"><h2>Comece pelo começo</h2></div>
                     <div class="caixaPerguntas">
-                    <input id="titulo" class="formatação" placeholder="Titulo do seu quizz">
-                    <input id="url" class="formatação" placeholder="URL da imagem do seu quizz">
-                    <input id="qtde" class="formatação"  placeholder="Quantidade de perguntas do quizz">
-                    <input id="nivel" class="formatação" placeholder="Quantidade de níveis do quizz">
+                        <input id="titulo" class="formatação" placeholder="Titulo do seu quizz">
+                        <input id="url" class="formatação" placeholder="URL da imagem do seu quizz">
+                        <input id="qtde" class="formatação"  placeholder="Quantidade de perguntas do quizz">
+                        <input id="nivel" class="formatação" placeholder="Quantidade de níveis do quizz">
                     </div>
                     <div class="rodape">
-                    <button onclick="capturarInfosComeco(this)"><h1>Prosseguir para criar perguntas</h1></button>
+                        <button onclick="capturarInfosComeco(this)"><h1>Prosseguir para criar perguntas</h1></button>
                     </div>`
 }
                
 function paginaPerguntas(){
     pag1.innerHTML="";
-    pag1.innerHTML += `<div class="topo"><h1>BuzzQuizz</h1></div>
+    pag1.innerHTML += `
                         <div class="enunciado"><h2>Crie suas perguntas</h2></div>
                         <div class="caixaPerguntas"></div>`;
 
@@ -47,37 +50,37 @@ function paginaPerguntas(){
                         
                         `
             }
-                  pag1.innerHTML +=     `<div class="rodape">
-                    <button onclick="validarDados(this)"><h1>Prosseguir para criar níveis</h1></button>
-                    </div>`
+                  pag1.innerHTML += `   <div class="rodape">
+                                            <button onclick="validarDados(this)"><h1>Prosseguir para criar níveis</h1></button>
+                                        </div>`
 }
 
 function paginaNiveis(){
     let pag3 = document.querySelector('.paginaQuizz');
-    pag3.innerHTML += `<div class="topo"><h1>BuzzQuizz</h1></div>
+    pag3.innerHTML += `
                         <div class="enunciado"><h2>Agora,decida os níveis!</h2></div>
                         <div class="caixaPerguntas">
-                        <h2>Nível 1</h2>
-                        <input id="campo1" placeholder="Título do nível">
-                        <input id="campo1" placeholder="% de acerto mínima">
-                        <input id="campo1" placeholder="URL da imagem do nível">
-                        <input id="campo5" placeholder="Descrição do nível">
+                            <h2>Nível 1</h2>
+                            <input id="campo1" placeholder="Título do nível">
+                            <input id="campo1" placeholder="% de acerto mínima">
+                            <input id="campo1" placeholder="URL da imagem do nível">
+                            <input id="campo5" placeholder="Descrição do nível">
                         </div>
                         <div class="rodape">
-                    <button onclick="validarDados(this)"><h1>Finalizar quiz</h1></button>
-                    </div>`
+                            <button onclick="validarDados(this)"><h1>Finalizar quiz</h1></button>
+                        </div>`
 }
 
 function paginaPronto(){
     let pag4 = document.querySelector('.paginaQuizz');
-    pag4.innerHTML += `<div class="topo"><h1>BuzzQuizz</h1></div>
+    pag4.innerHTML += `
                         <div class="enunciado"><h2>Seu quiz esta pronto!</h2></div>
                         <div class="caixaPerguntas">
-                    </div>
+                        </div>
                         <div class="rodape">
-                    <button onclick="validarDados(this)"><h1>Acessar quizz</h1></button>
-                   <div><h1>Voltar para home</h1></div>
-                    </div>`
+                            <button onclick="validarDados(this)"><h1>Acessar quizz</h1></button>
+                        <div><h1>Voltar para home</h1></div>
+                        </div>`
 }
 
 function capturarInfosComeco(){
@@ -112,7 +115,7 @@ function capturarInfosComeco(){
 
 
 function validarURL(urlValue){
-    var urlregex = new RegExp("^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
+    let urlregex = new RegExp("^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
     if (urlregex.test(urlValue)) {
         return (true);
     }
@@ -127,7 +130,7 @@ function CapturarInfosPerguntas(){
     urlValue= document.getElementById("urlPergunta").value;
 }
 
-paginaComeco();
+//paginaComeco();
 //paginaNiveis();
 //paginaPronto();
 

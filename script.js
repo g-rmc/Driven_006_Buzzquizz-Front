1768a87a991045e518b3ca9f1b9e2950528231b0
guilherme <guilhermepolicarpio@gmail.com>
2022-06-29 14:14:46 -0300

    Validação de lógica javascript na pagina 1 e geração de perguntas dinamicas na pagina 2

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function paginaComeco(){
-
 let pag1 = document.querySelector('.paginaQuizz');
+var tituloValue=undefined;
+var urlValue=undefined;
+var qtdeValue=undefined;
+var nivelValue=undefined;
+
+function paginaComeco(){
 pag1.innerHTML += `<div class="topo"><h1>BuzzQuizz</h1></div>
                     <div class="enunciado"><h2>Comece pelo começo</h2></div>
                     <div class="caixaPerguntas">
@@ -15,11 +19,14 @@ pag1.innerHTML += `<div class="topo"><h1>BuzzQuizz</h1></div>
 }
                
 function paginaPerguntas(){
-    let pag2 = document.querySelector('.paginaQuizz');
-    pag2.innerHTML += `<div class="topo"><h1>BuzzQuizz</h1></div>
+    pag1.innerHTML="";
+    pag1.innerHTML += `<div class="topo"><h1>BuzzQuizz</h1></div>
                         <div class="enunciado"><h2>Crie suas perguntas</h2></div>
-                        <div class="caixaPerguntas">
-                        <h2>Pergunta 1</h2>
+                        <div class="caixaPerguntas">`;
+
+    for(let i=1; i<=qtdeValue;i++){
+        pag1.innerHTML += `
+                        <h2>Pergunta ${i}</h2>
                         <input id="campo1" placeholder="Texto da pergunta">
                         <input id="campo2" placeholder="Cor de fundo da pergunta">
                         <h2>Resposta Correta</h2>
@@ -35,8 +42,9 @@ function paginaPerguntas(){
 
                         <input id="campo1" placeholder="Resposta incorreta 3">
                         <input id="campo2" placeholder="URL da imagem 3">
-                        </div>
-                        <div class="rodape">
+                        </div>`
+            }
+                  pag1.innerHTML +=     `<div class="rodape">
                     <button onclick="validarDados(this)"><h1>Prosseguir para criar níveis</h1></button>
                     </div>`
 }
@@ -70,13 +78,12 @@ function paginaPronto(){
 }
 
 function capturarInfosComeco(){
-    let tituloValue = document.getElementById("titulo").value;
-    let urlValue = document.getElementById("url").value;
-    let qtdeValue = document.getElementById("qtde").value;
-    let nivelValue = document.getElementById("nivel").value;
+    console.log("oi");
+    tituloValue = document.getElementById("titulo").value;
+    urlValue = document.getElementById("url").value;
+    qtdeValue = document.getElementById("qtde").value;
+    nivelValue = document.getElementById("nivel").value;
   
-
-
     if(tituloValue.length<20 || tituloValue>65){
         alert("Titulo deve ter entre 20 e 65 caracteres");
         paginaComeco();
@@ -96,7 +103,10 @@ function capturarInfosComeco(){
         alert("Insira uma URL valida");
         paginaComeco();
     }
-}
+    else{
+        console.log("oi");
+    paginaPerguntas();
+}}
 
 function validarURL(urlValue){
     var urlregex = new RegExp("^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
@@ -107,6 +117,5 @@ function validarURL(urlValue){
 }
 
 paginaComeco();
-//paginaPerguntas();
 //paginaNiveis();
 //paginaPronto();
